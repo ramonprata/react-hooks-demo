3f66ef79da85b33e74b3106104f4b30930624c65
Ramon Prata <ramon.prata@dtidigital.com.br>
2019-07-18 15:47:39 -0300

    hooks-1

--- src/hooksDemo/formWithHooks.jsx
+++ src/hooksDemo/formWithHooks.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, CardHeader, CardContent } from '@material-ui/core';
 import Input from './input';
 import WidthWindow from './widthWindow';
 import '../demo.css';
-// import {  } from './customHooks';
 
 const FormWithHooks = props => {
+  const [name, setName] = useState('');
+  const handleNameChange = e => {
+    setName(e.target.value);
+  };
+
+  const [surname, setSurname] = useState('');
+  const handleSurnameChange = e => {
+    setSurname(e.target.value);
+  };
+
   return (
     <Card className="card">
       <CardHeader title="Form with hooks" />
       <CardContent>
-        <Input label="Name" value="" onChange={() => {}} />
-        <Input label="Surname" value="" onChange={() => {}} />
+        <Input label="Name" value={name} onChange={handleNameChange} />
+        <Input label="Surname" value={surname} onChange={handleSurnameChange} />
         <WidthWindow width={0} />
       </CardContent>
     </Card>
--- src/hooksDemo/formWithNoHooks.jsx
+++ src/hooksDemo/formWithNoHooks.jsx
@@ -5,13 +5,31 @@ import Input from './input';
 import WidthWindow from './widthWindow';
 
 class FormWithNoHooks extends Component {
+  state = {
+    name: '',
+    surname: ''
+  };
+
+  handleNameChange = e => {
+    this.setState({
+      name: e.target.value
+    });
+  };
+
+  handleSurnameChange = e => {
+    this.setState({
+      surname: e.target.value
+    });
+  };
+
   render() {
+    const { name, surname } = this.state;
     return (
       <Card className="card">
         <CardHeader title="Form with no hooks" />
         <CardContent>
-          <Input label="Name" value={''} onChange={() => {}} />
-          <Input label="Surname" value={''} onChange={() => {}} />
+          <Input label="Name" value={name} onChange={this.handleNameChange} />
+          <Input label="Surname" value={surname} onChange={this.handleSurnameChange} />
           <WidthWindow width={0} />
         </CardContent>
       </Card>
